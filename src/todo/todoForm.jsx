69981d09702efcd757c3dffef2f81ad9cb0a9e2e
jpvieira1 <jp201101@gmail.com>
2022-04-01 11:21:12 -0300

    Estrutura do Formulário

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 export default props => (
-    <div>
-        <h1>form</h1>
+    <div role='form' className="todoForm">
+        <div className="col-xs-12 col-sm-9 col-md-10">
+        <input id="description" className="form-control" 
+        placeholder="Adicione uma tarefa"/>
+        </div>
+        <div className="col-xs-12 col-sm-3 col-md-2">
+            <button className="btn btn-primary"><i className="fa fa-plus"></i></button>
+        </div>
     </div>
 )
