541c6c116f377a657bce2cc9f51a4c2469aa5a5e
jpvieirah <jp201101@gmail.com>
2022-04-05 11:04:18 -0300

    Melhorias de CSS e Limpar Formulário

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -15,6 +15,7 @@ export default class Todo extends Component {
 
         this.handleChange = this.handleChange.bind(this)
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleClear = this.handleClear.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
         this.handleRemove = this.handleRemove.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
@@ -65,6 +66,10 @@ export default class Todo extends Component {
         .then(resp => this.refresh(this.state.description))
     }
 
+    handleClear() {
+        this.refresh()
+    }
+
     render() {
         return (
             <div>
@@ -73,6 +78,7 @@ export default class Todo extends Component {
                 handleChange={this.handleChange}
                 handleAdd={this.handleAdd}
                 handleSearch={this.handleSearch}
+                handleClear={this.handleClear}
                 />
                 <TodoList list={this.state.list} 
                 handleRemove={this.handleRemove}
